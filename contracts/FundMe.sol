1530997d0fbe4fe8d73b9c8419dc3c5cd73f096b
balachandarDeve <balachandariosdev@gmail.com>
2024-10-22 21:36:25 +0530

    More functions

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -14,7 +14,19 @@ contract FundMe {
         peopleList.push(People(_id, _name));
     }
 
-    function getPeople(uint256 index) public view returns (People) {
+    function getPeople(uint256 index) public view returns (People memory) {
         return peopleList[index];
     }
+
+    function deleteAtIndex(uint256 index) public {
+        if (peopleList.length > index) {
+        delete peopleList[index];
+        }
+    }
+
+    function getCount() public view returns(uint256) {
+        return  peopleList.length;
+    }
+
+
 }
